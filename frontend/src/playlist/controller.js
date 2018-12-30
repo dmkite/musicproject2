0eb6cc690a2ecc5d82261c675c8efd657f6c33dd
Dylan <kite.d92@gmail.com>
2018-12-30 15:03:53 -0800

    added songs to playlist screen, playlist css

--- frontend/src/playlist/controller.js
+++ frontend/src/playlist/controller.js
@@ -1,14 +1,17 @@
 const model = require('./model')
 const {signout} = require('../login-signup/controller')
+const songModel = require('../music-project/hits/model')
+const {favSongTemplate} = require('../music-project/hits/view')
 
 function init(){
-    document.querySelector('#playlist').onkeyup = checkVals
-    document.querySelector('#playlist').onsubmit = function (e) { makePlaylist(e) }
+    document.querySelector('#playlistForm').onkeyup = checkVals
+    document.querySelector('#playlistForm').onsubmit = function (e) { makePlaylist(e) }
+    addSongs()
 }
 
 function checkVals(){
-    const input = document.querySelector('#playlist input')
-    const button = document.querySelector('#playlist button')
+    const input = document.querySelector('#playlistForm input')
+    const button = document.querySelector('#playlistForm button')
     if(input.value === '') button.setAttribute('disabled', 'disabled')
     else button.removeAttribute('disabled')
 }
@@ -20,8 +23,8 @@ function makePlaylist(e){
     .then(result =>{
         if (!result) throw Error
         localStorage.setItem('spotify_playlist_id', result.spotify_playlist_id)
-        let playlistName = document.querySelector('#playlist input').value
-        document.querySelector('#playlist').remove()
+        let playlistName = document.querySelector('#playlistForm input').value
+        document.querySelector('#playlistForm ').remove()
         document.querySelector('main').textContent = `Playlist ${playlistName} created`
         
     })
@@ -34,10 +37,10 @@ function makePlaylist(e){
 
 function generateBody(){
     body = {
-        name: document.querySelector('#playlist input').value,
+        name: document.querySelector('#playlistForm input').value,
         public: false,
         collaborative: false,
-        description: document.querySelector('#playlist textarea').value,
+        description: document.querySelector('#playlistForm textarea').value,
         user_id: localStorage.getItem('spotifyId'),
         access_token: localStorage.getItem('access_token'),
         userId: localStorage.getItem('userId')
@@ -45,5 +48,16 @@ function generateBody(){
     return body
 }
 
+function addSongs(){
+    return songModel.all()
+    .then(result => {
+        console.log(result)
+        const songHTML = result.data.map(item => favSongTemplate(item))
+        document.querySelector('.trackSelect').innerHTML = songHTML.join('')
+        const hits = document.querySelectorAll('.hits')
+        hits.forEach(item => item.innerHTML += '<input type="checkbox" checked>')
+    })
+    .catch(err => console.error(err))
+}
 
 module.exports = {init}
